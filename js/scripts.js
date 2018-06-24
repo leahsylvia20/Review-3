257be2b2343d2741ffebac5d0856c28aeb800e11
leahsylvia20 <leahsylvia@gmail.com>
2018-06-24 14:56:10 -0700

    add if, else if, else, function to scripts file, plus show results 15%, 5%, 3% and numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,14 @@ var game = function(input) {
 var pingpong = function(){
   number.map(function(numbers){
     $("#result").append("<li>" + "Your results:" + "<li>");
+  } else if (number % 15 === 0) {
+    $("#result").append("<li>" + "pingpong" + "</li>");
+  } else if (number % 5 === 0) {
+    $("#result").append("<li>" + "pong" + "</li>");
+  } else if (number % 3 === 0) {
+    $("#result").append("<li>" + "ping" + "</li>");
+  } else {
+    $("#result").append("<li>" + "numbers" + "</li>");
   }
 }
 
